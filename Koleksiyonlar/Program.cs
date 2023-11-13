e5310e794b2affc6926ad9fdcbb66c827e920421
egina <egina@outlook.de>
2023-11-13 22:53:30 +0100

    20 Ocak Ders Sonu

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koleksiyonlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Engin", "Murat", "Halil" };
+            //Console.WriteLine(isimler[0]);
+
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Halil" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            isimler2.Add("Ilker");
+            Console.WriteLine(isimler2[3]);
+            Console.WriteLine(isimler2[0]);
+        }
+    }
+}
