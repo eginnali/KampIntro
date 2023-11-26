83d8bc9cf97b3b4715c3afe7516659b64b05020e
egina <egina@outlook.de>
2023-11-26 19:55:18 +0100

    23 Ocak Ders Sonu

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    internal class BasvuruManager
+    {
+        // Method injection
+        public void BasvuruYap(IKrediManager krediManeger, List<ILoggerService> loggerServices)
+        {
+            // Basvuran bilgilerini degerlendirme
+            // 
+            krediManeger.Hesapla();
+            
+            foreach (var loggerService in loggerServices)
+            {
+                loggerService.Log();
+            }
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
---
+++ Oop2/CustomerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oop2
+{
+    // Is siniflari // business classes
+    internal class CustomerManager
+    {
+        public void Add(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+
+
+        }
+    }
+}
